e6e4ea5c020865da440d4c97a2cdce1bdf1c62fb
bvvinai <venkata.vinai@gmail.com>
2021-09-25 10:38:20 +0530

    checking

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './nav.module.css'
 
 // Assigned To Vinai
 // Background Colour - primaryBg
 
 function nav() {
     return (
-        <div>
-            NavBar kodkf odif kfdok fojdf od oj
+        <div id={styles.navbar}>
+            <Image src="/logo.png" height={50} width={100} />
+            <div className={styles.navbutton}>
+                <Link href="google.com">
+                    Events/Workshops
+                </Link>
+            </div>
+            <div className={styles.navbutton}>
+                <Link href="google.com">
+                    Teams
+                </Link>
+            </div>
+            <div className={styles.navbutton}>
+                <Link href="google.com">
+                    Podcasts
+                </Link>               
+            </div>
         </div>
     )
 }
