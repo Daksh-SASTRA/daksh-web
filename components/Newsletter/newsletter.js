26091894788521e97e3de5c35b82a52cbb7ea3b2
ravidindi <ravidindisaiprasannakumar@gmail.com>
2021-09-25 19:52:27 +0530

    added newsletter and statisticCards component

--- components/Newsletter/newsletter.js
+++ components/Newsletter/newsletter.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import styles from './newsletter.module.css'
 
 // Assigned To Sai Prasanna
 // Background Colour - secondaryBg
 
 function newsletter() {
     return (
-        <div>
-            NewsLetter
+        <div className={styles.maincontainer}>
+
+        <h3 >Stay Updated!</h3>
+        <div className={styles.container1}>
+           
+            <input type="email" name="myemail" id="" placeholder="Enter Email"/>
+            <button className={styles.btn}>Join NOW</button>
         </div>
+    </div>
     )
 }
+
 
 export default newsletter
--- components/StatisticCards/statisticCards.js
+++ components/StatisticCards/statisticCards.js
@@ -1,14 +1,46 @@
 import React from 'react'
+import styles from './statisticCards.module.css'
+import PropTypes from 'prop-types'
+
 
 // Assigned To Sai Prasanna
 // Background Colour - secondaryBg
 
-function statisticCards() {
+function statisticCards(props) {
+    const decorwhite=(s,i)=>{
+       return s.substring(0,i);
+    }
+    const decorgrey=(s,i)=>{
+       return s.substring(i,s.length);
+    }
     return (
-        <div>
-            Cards
-        </div>
+        <div className={styles.container}>
+            <div className={`${styles.item} ${styles.item1}`} >
+            <p>{decorwhite(props.s1,props.i1)}<br /><span>{decorgrey(props.s1,props.i1)}</span></p>
+        </div><div className={styles.item}>
+        <p>{decorwhite(props.s2,props.i2)}<br /><span>{decorgrey(props.s2,props.i2)}</span></p>
+            </div><div className={`${styles.item} ${styles.item3}`}>
+            <p>{decorwhite(props.s3,props.i3)}<br /><span>{decorgrey(props.s3,props.i3)}</span></p>
+            </div>
+    </div>
     )
 }
+statisticCards.propTypes = {
+    s1: PropTypes.string.isRequired,
+    s2: PropTypes.string.isRequired,
+    s3: PropTypes.string.isRequired,
+    i1: PropTypes.number.isRequired,
+    i2: PropTypes.number.isRequired,
+    i3: PropTypes.number.isRequired,
+  };
+  statisticCards.defaultProps = {
+    s1:"10+ Events",
+    s2:"100k Pool",
+    s3:"Lorem ipsum dolor.",
+    i1:3,
+    i2:4,
+    i3:5
+  };
+  
 
 export default statisticCards
