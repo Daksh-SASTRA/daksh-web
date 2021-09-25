4d59e46212f4370567229e6e2097ce188192f2e2
bvvinai <venkata.vinai@gmail.com>
2021-09-25 15:23:26 +0530

    Update podcastPage.js

--- components/PodcastPage/podcastPage.js
+++ components/PodcastPage/podcastPage.js
@@ -6,7 +6,7 @@ import styles from './podcastpage.module.css'
 function podcastPage() {
     return (
         <div>
-            <iframe src="https://player.vimeo.com/video/614273659?h=3472f51f43&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="288" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="ffdf.mp4"></iframe>
+            <iframe src="https://player.vimeo.com/video/614339947?h=8f513583b1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="480" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="music.mp4"></iframe>           
             <div></div>
         </div>
     )
