3adea669d54cab02af67a9f189afd4466be1f267
Neha Anand <neha280103@gmail.com>
2021-09-25 19:44:22 +0530

    structured about section

--- components/About/about.js
+++ components/About/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './about.module.css'
+import Image from 'next/image'
+import styles from './about.module.css'
 
 // Assigned To Neha
 // Background Colour - secondaryBg
@@ -7,9 +8,29 @@ import './about.module.css'
 
 function about() {
     return (
-        <div>
-            About
-        </div>
+        <section className={styles.about}>
+            <div className={`${styles.container__about}`}>
+                <div className={styles.row__about}>
+                    <div className={styles.col_lg_6}>
+                        <div className={styles.text_container__about}>
+                            <h3 className={styles.about__h3}>ABOUT US</h3>
+                            <p className={styles.about__p}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <a href="#">Teams</a>
+                        </div>
+                    </div>
+                    <div className={styles.col_lg_6}>
+                        <div className={styles.image_container__about}>
+                            <img className={styles.about__img_fluid}src="https://dummyimage.com/700x400/fff/000000&text=Daksh" alt="dummy image"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
