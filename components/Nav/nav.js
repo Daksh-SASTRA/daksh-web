d9f7b9d92cd587683ac0b10cd93acbc5a32d2bad
bvvinai <venkata.vinai@gmail.com>
2021-09-25 08:20:22 +0530

    new fork

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
+import styles from './nav.module.css'
 
 // Assigned To Vinai
 // Background Colour - primaryBg
 
 function nav() {
     return (
-        <div>
-            NavBar
+        <div id={styles.navbar}>
+            <Image src="/logo.png" height={50} width={100} />
         </div>
     )
 }
