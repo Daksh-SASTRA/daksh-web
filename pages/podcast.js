f188ef1d50c7afa374a577299e6b35e671682954
vishcomestrue <ramvishwa03@gmail.com>
2021-09-26 09:29:27 +0530

    Prevented overflow in X

--- pages/podcast.js
+++ pages/podcast.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.main} style={{overflowX: "hidden"}}>
         <Nav />
         <PodcastPage />
         <Footer />
