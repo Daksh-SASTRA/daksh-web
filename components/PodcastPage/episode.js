78d506c6260c3975e3e4a5108941de26e65e35bc
bvvinai <venkata.vinai@gmail.com>
2021-09-26 06:43:55 +0530

    episode item updated

--- components/PodcastPage/episode.js
+++ components/PodcastPage/episode.js
@@ -1,13 +1,17 @@
 import styles from './podcastpage.module.css'
+import {useState} from 'react'
 
 function episode(props){
     
+    const [hover, onHover] = useState(false);
+
     return (
         <>
-            <div className={styles.episode} onMouseEnter={()=>{}}>
-                <img src={props.imageurl} />
-                <div className={styles.overlay}>
+            <div className={styles.episode} onMouseEnter={()=>{onHover(true);}}  onMouseLeave={()=>{onHover(false);}}> 
+                <img src={props.imageurl} className={styles.eimg}/>
+                <div className={hover?styles.overlayEx:styles.overlay}>
                     {props.title}
+                    <button className={hover?styles.watchnow:styles.hide}>Watch Now</button>
                 </div>
             </div>
         </>
