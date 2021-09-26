ea651bef2d9fd56e531822850d68eed51adff86f
vishcomestrue <ramvishwa03@gmail.com>
2021-09-26 10:50:11 +0530

    Fine-tuned the styles for the page and added new classes and properties

--- components/PodcastPage/podcastPage.js
+++ components/PodcastPage/podcastPage.js
@@ -2,16 +2,24 @@
 // Background Colour - primaryBg
 
 import styles from './podcastpage.module.css'
-import Episode from './episode'
-
+import Episode from './Episode'
 
 function podcastPage() {
     return (
-        <div style={{padding: "10vh 0 0 0"}}>
+        <div style={{padding: "10vh 0 0 0", display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <div id={styles.podpage}>
-                <iframe src="https://player.vimeo.com/video/614339947?h=8f513583b1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width={640} height={480} frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="music.mp4"></iframe>
+                <iframe id={styles.iframe} src="https://player.vimeo.com/video/614339947?h=8f513583b1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width={640} height={480} frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="music.mp4"></iframe>
+                <div className={styles.podcontext}>
+                    <h1>Latest Episode</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores veniam recusandae est, eius nisi nobis? Quisquam nostrum repellat sequi tempore!</p>
+                    <button className={styles.button}>Listen now 🎙</button>
+                </div>
             </div>
+            <h1 id={styles.allepi}>All Episodes</h1>
             <div id={styles.grid}>
+                <Episode title={"name"} desc={"Lorem Ipsum"} imageurl={"https://picsum.photos/300/200"}/>
+                <Episode title={"name"} desc={"Lorem Ipsum"} imageurl={"https://picsum.photos/300/200"}/>
+                <Episode title={"name"} desc={"Lorem Ipsum"} imageurl={"https://picsum.photos/300/200"}/>
                 <Episode title={"name"} desc={"Lorem Ipsum"} imageurl={"https://picsum.photos/300/200"}/>
                 <Episode title={"name"} desc={"Lorem Ipsum"} imageurl={"https://picsum.photos/300/200"}/>
             </div>
