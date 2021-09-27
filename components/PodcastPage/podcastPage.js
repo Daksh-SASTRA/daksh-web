4e8d4053b21e09292e8628aee56de85bc8ee35a8
bvvinai <venkata.vinai@gmail.com>
2021-09-27 21:01:42 +0530

    Update podcastPage.js

--- components/PodcastPage/podcastPage.js
+++ components/PodcastPage/podcastPage.js
@@ -8,7 +8,6 @@ function PodcastPage() {
     const [desc, setDesc] = useState("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores veniam recusandae est, eius nisi nobis? Quisquam nostrum repellat sequi tempore!");
     const [latestPage, setPage] = useState(true);
     var url = "https://player.vimeo.com/video/"+videoid+"?h=8f513583b1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479";
-    console.log(url);
     var page = <iframe id={styles.iframe} src={url} frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="music.mp4"></iframe>
                 
     if(latestPage){
