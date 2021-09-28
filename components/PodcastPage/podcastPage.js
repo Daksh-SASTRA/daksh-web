4b0b39d830d995ea0be425bb52576e26485b1e34
bvvinai <venkata.vinai@gmail.com>
2021-09-28 10:34:34 +0530

    iframe updates

--- components/PodcastPage/podcastPage.js
+++ components/PodcastPage/podcastPage.js
@@ -8,9 +8,11 @@ function PodcastPage() {
     const [desc, setDesc] = useState("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores veniam recusandae est, eius nisi nobis? Quisquam nostrum repellat sequi tempore!");
     const [latestPage, setPage] = useState(true);
     var url = "https://player.vimeo.com/video/"+videoid+"?h=8f513583b1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479";
-    console.log(url);
-    var page = <iframe id={styles.iframe} src={url} frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="music.mp4"></iframe>
-                
+   
+    var page =  <div className={styles.videoWrapper}>
+                    <iframe id={styles.video} src={url} frameBorder="0" allowFullScreen></iframe>
+                </div>
+
     if(latestPage){
         page = <div className={styles.poster}>
                     <h1 id={styles.posterTitle}>Latest Title</h1>
@@ -19,11 +21,13 @@ function PodcastPage() {
     }
 
     return (
-        <div style={{padding: "100vh 0px 0px 0px", display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            {page}
-            <div className={styles.podcontext}>
-                <h1 id={styles.titleFrame}>{title}</h1>
-                <p id={styles.descFrame}>{desc}</p>
+        <div style={{padding: "10vh 0px 0px 0px", display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <div id={styles.podpage}>               
+                {page}
+                <div className={styles.podcontext}>
+                    <h1 id={styles.titleFrame}>{title}</h1>
+                    <p id={styles.descFrame}>{desc}</p>
+                </div>
             </div>
             <h1 id={styles.allepi}>All Episodes</h1>
             <div id={styles.grid}>
