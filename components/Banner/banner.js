46e09e3eccecdd300f120932c72031a9eefd32f8
SivapriyaJayakumar <68330216+SivapriyaJayakumar@users.noreply.github.com>
2021-09-28 13:38:55 +0530

    Banner and Cards sivapriya (#8)
    Banner and Cards Section added

--- components/Banner/banner.js
+++ components/Banner/banner.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import styles from './banner.module.css'
 
 // Assigned To Sivapriya
 // Background Colour - primaryBg
 
 function banner() {
+    
     return (
-        <div>
-            Banner
+        <>
+        <div className={styles.topcover}>
+        <div className={styles.divwrap}>
+           <div className={styles.divbanner}>
+               <div className={styles.divbanner__headtext}>
+                   DAKSH IS HAPPENING
+               </div>
+               <div className={styles.divbanner__descriptiontext}>
+                   BUILD THINGS THAT MATTER
+               </div>
+               <div className={styles.divbanner__eveandwsbtn}>
+                 Events and Workshops
+               </div>
+              
+           </div>
+           <div className={styles.divbannerfadebtm}>
+                    
+            </div>
+          
         </div>
+      
+       
+       
+    </div>
+   
+   </>
     )
 }
 
--- components/BannerCards/bannerCards.js
+++ components/BannerCards/bannerCards.js
@@ -1,14 +1,99 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import styles from './bannercards.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 // Assigned To Sivapriya
 // Background Colour - primaryBg
 
 function bannerCards() {
-    return (
-        <div>
-            Banner Cards
+    const carousel_items=[
+      {
+        idx:0,
+        description:"Events",
+        src:'/tech.png',
+        buttonText:'view'
+      },
+      {
+        idx:1,
+        description:"Workshops",
+        src:'/tech.png',
+        buttonText:'view'
+      },
+      {
+        idx:2,
+        description:"Podcasts",
+        src:'/tech.png',
+        buttonText:'view'
+      }
+    ]
+
+    const [imageIdx,setImageIdx]=useState(0);
+    
+    var settings = {
+       
+        className: "center",
+        centerMode: true,
+        infinite:true,
+        slidesToShow: 3,
+        speed: 500,
+        beforeChange:(current,next)=>setImageIdx(next),
+        
+        responsive: [
+            {
+              breakpoint: 770,
+              settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 1
+              },
+
+              
+            },
+        ]
+       
+      };
+
+   return(   
+       <>
+     
+       
+       <div className={styles.cardRenderWrap}>
+      
+       <div className={styles.cardcarousel} id="cardcarousel">
+           
+            <Slider {...settings} >
+                {carousel_items.map((item,idx)=>{
+                  return(
+                   <div className={styles.card} key={idx}>
+                   <div className={styles.card__vectorimg}>
+                       <img className={styles.vector} src={item.src}/>
+                   </div>
+                   <div className={styles.card__head}>{item.description}</div>
+                   <div className={styles.card__viewbtn}>{item.buttonText}</div>
+               </div>
+                  )
+                })}               
+              </Slider>
+             
+            
         </div>
-    )
+       
+        </div>
+    
+     </>
+    );
+   
 }
 
-export default bannerCards
+export default bannerCards;
