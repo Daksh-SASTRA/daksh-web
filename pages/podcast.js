a73efb264f6253c8826cdd7c40b11f8664d63ad5
vishcomestrue <ramvishwa03@gmail.com>
2021-09-29 19:25:51 +0530

    Removed 'Overflow-X'

--- pages/podcast.js
+++ pages/podcast.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main} style={{overflowX: "hidden"}}>
+      <main className={styles.main}>
         <Nav />
         <PodcastPage />
         <Footer />
