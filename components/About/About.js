6155669fff7d9810388ce984a522cb2941bacbeb
itherohit <rohitshan751@gmail.com>
2021-10-09 14:34:07 +0530

    Modified StaticCard styles

--- components/About/About.js
+++ components/About/About.js
@@ -11,7 +11,7 @@ function About() {
                     <div className={styles.text_container__about}>
                         <h3 className={styles.about__h3}>About Us</h3>
                         <p className={styles.about__p} data-aos="fade-up" >
-                        Instituted in 2007, DAKSH is the annual techno management fest of Sastra University. Ever since its establishm ent it has been a platform for a multitude of students and aspirants to unravel their talents and showcase their technical prowess. We’ve grown together as a fest since our inception providing students with the finest exposure and skill. This edition of Daksh aims at creating a virtual forum bringing together the eccentric minded and celebrating technology.
+                        Instituted in 2007, DAKSH is the annual techno management fest of Sastra University. Ever since its establishment it has been a platform for a multitude of students and aspirants to unravel their talents and showcase their technical prowess. We’ve grown together as a fest since our inception providing students with the finest exposure and skill. This edition of Daksh aims at creating a virtual forum bringing together the eccentric minded and celebrating technology.
                         </p>
                         {/* <Link href="#newsletter">
                             <a className={styles.button__about} >  Teams </a>
