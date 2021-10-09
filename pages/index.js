097669137132e2db632f6610a0c403b5bdb0ba92
San <74140867+santhosh847@users.noreply.github.com>
2021-10-09 12:48:00 +0530

    Floating icons (#19)

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Banner from '../components/Banner/Banner'
-import BannerCards from '../components/BannerCards/BannerCards'
-import About from '../components/About/About'
-import Sponsers from '../components/Sponsers/Sponsers'
-import Podcast from '../components/Podcast/Podcast'
-import StatisticCards from '../components/StatisticCards/StatisticCards'
-import Newsletter from '../components/Newsletter/Newsletter'
-import Nav from '../components/Nav/Nav'
-import Footer from '../components/Footer/Footer'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Banner from "../components/Banner/Banner";
+import BannerCards from "../components/BannerCards/BannerCards";
+import About from "../components/About/About";
+import Sponsers from "../components/Sponsers/Sponsers";
+import Podcast from "../components/Podcast/Podcast";
+import StatisticCards from "../components/StatisticCards/StatisticCards";
+import Newsletter from "../components/Newsletter/Newsletter";
+import Nav from "../components/Nav/Nav";
+import Footer from "../components/Footer/Footer";
+import FloatingIcons from "../components/FloatingIcons/FloatingIcons";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Daksh SASTRA</title>
-        <meta name="description" content="The international techno-management festival of SASTRA University" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Daksh SASTRA</title>
+				<meta
+					name="description"
+					content="The international techno-management festival of SASTRA University"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <Nav />
-        <Banner />
-        <BannerCards />
-        <About />
-        <Sponsers />
-        <Podcast />
-        <StatisticCards/>
-        <Newsletter />
-        <Footer />
-      </main>
-    </div>
-  )
+			<main className={styles.main}>
+				<FloatingIcons />
+				<Nav />
+				<Banner />
+				<BannerCards />
+				<About />
+				<Sponsers />
+				<Podcast />
+				<StatisticCards />
+				<Newsletter />
+				<Footer />
+			</main>
+		</div>
+	);
 }
