c02cc6b21de36a82db77b2e0dd32db8d6e198952
VISHWANATH R <84440397+vishcomestrue@users.noreply.github.com>
2021-10-11 08:50:18 +0530

    Added Contact page (#23)

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,9 +1,54 @@
 import React from 'react'
 
+// Link was imported to test Link tag for emails but instead a tag was used since Link tag didnt get the custom styles
+import Link from 'next/link'  
+
+import styles from './contact.module.css'
+
 function Contact() {
     return (
-        <div>
-            Contact Page
+        <div className={`${styles.container} ${styles.flexCenter}`}>
+            <div className={`${styles.contactHeadingMain} ${styles.flexCenter}`}>
+                C O N T A C T S
+            </div>
+            <div className={`${styles.contactContent} ${styles.flexCenter}`}>
+                <div className={`${styles.contacts} ${styles.flexCenter}`}>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Manthrini P</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Student Coordinator</span> 
+                    <a href="mailto:coordinator.daksh@sastra.ac.in" className={styles.email}>coordinator.daksh@sastra.ac.in</a>
+                    <span className={styles.phnNo}>+91 9884822554</span>
+                </div>
+                <div className={`${styles.contacts} ${styles.flexCenter}`}>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Hariharan M</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Student Secretary</span> 
+                    <a href="mailto:chairman.daksh@sastra.ac.in" className={styles.email}>chairman.daksh@sastra.ac.in</a>   
+                    <span className={styles.phnNo}>+91 7397067957</span>
+                </div>
+                <div className={`${styles.contacts} ${styles.flexCenter}`}>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Lalitha K</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Overall Technical Coordinator</span> 
+                    <a href="mailto:lalitha.otcdaksh@gmail.com" className={styles.email}>lalitha.otcdaksh@gmail.com</a>   
+                    <span className={styles.phnNo}>+91 6379603262</span>
+                </div>
+                <div className={`${styles.contacts} ${styles.flexCenter}`}>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Harish Aravinda P</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Treasurer</span> 
+                    <a href="mailto:finance.daksh@sastra.ac.in" className={styles.email}>finance.daksh@sastra.ac.in</a>   
+                    <span className={styles.phnNo}>+91 9841419109</span>
+                </div>
+                <div className={`${styles.contacts} ${styles.flexCenter}`}>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Muthu Solayappan</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Head, Promotions and Outreach</span> 
+                    <a href="mailto:promotions.daksh@sastra.ac.in" className={styles.email}>promotions.daksh@sastra.ac.in</a>   
+                    <span className={styles.phnNo}>+91 9840495111</span>
+                </div>
+                <div className={`${styles.contacts} ${styles.flexCenter}`}>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Harsha Vardhan H</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Head, Marketing and Shows</span> 
+                    <a href="mailto:marketing.daksh@sastra.ac.in" className={styles.email}>marketing.daksh@sastra.ac.in</a>   
+                    <span className={styles.phnNo}>+91 7395958123</span>
+                </div>
+            </div>
         </div>
     )
 }
