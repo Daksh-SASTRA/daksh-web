1705e9aa67d768db6d04c2b84208a9fc87746402
Neha Anand <70203777+NehaAnand28@users.noreply.github.com>
2021-10-11 08:49:26 +0530

    Sponsor images added (#22)
    * added sponser images folder in public
    
    * img src added for all slider elements
    
    * added border-right to img
    
    * added padding and border-color

--- components/Sponsers/Sponsers.js
+++ components/Sponsers/Sponsers.js
@@ -3,6 +3,7 @@ import styles from './sponsers.module.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Image from 'next/image'
 
 function Sponsers() {
     var settings = {
@@ -13,6 +14,7 @@ function Sponsers() {
         autoplaySpeed: 1000,
         variableWidth: true,
         speed: 500,
+        mobileFirst:true,
         responsive: [
             {
               breakpoint: 770,
@@ -54,20 +56,29 @@ function Sponsers() {
                 </div> */}
                 <div className={styles.sponser_slider}>
                     <Slider {...settings} >
-                        <img className={styles.sponser_infinite} src="https://picsum.photos/300/200"  alt="sponser-1"/>
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/300/200"  alt="sponser-2" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/900/300"  alt="sponser-3" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/200/200"  alt="sponser-4" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/400/200"  alt="sponser-5" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/500/200"  alt="sponser-6" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/100/200"  alt="sponser-7" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/800/200"  alt="sponser-8" />
+                        {/* <img className={styles.sponser_infinite} src="https://picsum.photos/300/200"  alt="sponser-1"/> */}
+                        {/* <Image className={styles.sponser_infinite} src="/sponsers/A&S_creations-logo.jpg" alt="sponser-1" width="300" height="200" />
+                        <Image  className={styles.sponser_infinite} src="/sponsers/Algorythm-logo.png" alt="sponser-2" width="300" height="200" />
+                        <Image className={styles.sponser_infinite} src="/sponsers/Coding-Blocks-logo.png"  alt="sponser-3" width="300" height="200" />
+                        <Image className={styles.sponser_infinite} src="/sponsers/edvoy-logo.png" alt="sponser-4" width="300" height="200" />
+                        <Image className={styles.sponser_infinite} src="/sponsers/MCD-logo.png"  alt="sponser-5" width="300" height="200" />
+                        <Image className={styles.sponser_infinite} src="/sponsers/ZAPP-logo.png" alt="sponser-6" width="300" height="200" />
+                        <Image className={styles.sponser_infinite} src="/sponsers/ZM-logo.png"  alt="sponser-7" width="300" height="200" /> */}
+
+                        <img className={styles.sponser_infinite} src="/sponsers/A_S_creations-logo.png"   alt="sponser-1"/>
+                        <img className={styles.sponser_infinite}  src="/sponsers/Algorythmlogo.png"   alt="sponser-2" />
+                        <img className={styles.sponser_infinite}  src="/sponsers/CodingBlocks-logo.png"    alt="sponser-3" />
+                        <img className={styles.sponser_infinite}  src="/sponsers/edvoy-logo.png"    alt="sponser-4" />
+                        <img className={styles.sponser_infinite}  src="/sponsers/MCD-logo.png"  alt="sponser-5" />
+                        <img className={styles.sponser_infinite}  src="/sponsers/ZAPPs-logo.png"   alt="sponser-6" />
+                        <img className={styles.sponser_infinite}  src="/sponsers/ZM-logo.png"  alt="sponser-7" />
+                        {/* <img className={styles.sponser_infinite}  src="https://picsum.photos/800/200"  alt="sponser-8" />
                         <img className={styles.sponser_infinite}  src="https://picsum.photos/200/200"  alt="sponser-9" />
                         <img className={styles.sponser_infinite}  src="https://picsum.photos/700/200"  alt="sponser-10" />
                         <img className={styles.sponser_infinite}  src="https://picsum.photos/300/500"  alt="sponser-11" />
                         <img className={styles.sponser_infinite}  src="https://picsum.photos/500/300"  alt="sponser-12" />
                         <img className={styles.sponser_infinite}  src="https://picsum.photos/200/200"  alt="sponser-13" />
-                        <img className={styles.sponser_infinite}  src="https://picsum.photos/400/200"  alt="sponser-14" />
+                        <img className={styles.sponser_infinite}  src="https://picsum.photos/400/200"  alt="sponser-14" /> */}
                     </Slider>
                 </div>
         </section>
