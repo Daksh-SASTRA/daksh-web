8d399b90787a1652e69943d58980d5b46a4480cd
itherohit <rohitshan751@gmail.com>
2021-11-27 11:31:12 +0530

    Loader added

--- pages/_app.js
+++ pages/_app.js
@@ -19,11 +19,9 @@ function MyApp({ Component, pageProps }) {
     });
 
     const start = () => {
-      console.log("start");
       setLoading(true);
     };
     const end = () => {
-      console.log("findished");
       setLoading(false);
     };
     Router.events.on("routeChangeStart", start);
