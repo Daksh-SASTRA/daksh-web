c67e03667ab3a72076a56fa6037ffec9c0f73280
Deepakh Srinivasan <92135486+debugnivas@users.noreply.github.com>
2021-11-27 11:09:15 +0530

    :sparkles: :art: Add loader (#30)
    * 🔗 Connected Podcasts Page to the Vimeo API
    
    * ✅ Added Favicon, titles and descriptions for pages
    
    * :sparkles: :art: Add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,41 @@
-import '../styles/globals.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import {useEffect} from 'react';
+import React from "react";
+import "../styles/globals.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Router from "next/router";
+import Loader from "../components/Loader/Loader";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(()=>{
-    AOS.init({    
-      offset: 100, 
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
       delay: 0,
       duration: 1000,
-      easing: 'ease',
+      easing: "ease",
       once: true,
       mirror: false,
-      anchorPlacement: 'top-bottom',
+      anchorPlacement: "top-bottom",
     });
-  },[])
-  return <Component {...pageProps} />
+
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+  return <>{loading ? <Loader /> : <Component {...pageProps} />}</>;
 }
 
-export default MyApp
+export default MyApp;
