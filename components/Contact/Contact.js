31438eb477d9652334042cf503b3030efac878f6
ravidindi <ravidindisaiprasannakumar@gmail.com>
2022-09-28 12:09:13 +0530

    minor changes in order

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -19,20 +19,14 @@ function Contact() {
                     {/* <span className={styles.phnNo}>+91 9884822554</span> */}
                 </div>
                 <div className={`${styles.contacts} ${styles.flexCenter}`} data-aos="fade-up" data-aos-delay="100">
-                    <span className={`${styles.name} ${styles.pd-5}`}>Ajai K R</span>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Ajai Balaganesh</span>
                     <span className={`${styles.designation} ${styles.pd-5}`}>Student Secretary</span> 
                     <a href="mailto:chairman.daksh@sastra.ac.in" className={styles.email}>chairman.daksh@sastra.ac.in</a>   
                     {/* <span className={styles.phnNo}>+91 7397067957</span> */}
                 </div>
-                <div className={`${styles.contacts} ${styles.flexCenter}`} data-aos="fade-up"  data-aos-delay="200">
-                    <span className={`${styles.name} ${styles.pd-5}`}>Achint Patro</span>
-                    <span className={`${styles.designation} ${styles.pd-5}`}>Overall Technical Coordinator</span> 
-                    <a href="mailto:lalitha.otcdaksh@gmail.com" className={styles.email}>lalitha.otcdaksh@gmail.com</a>   
-                    {/* <span className={styles.phnNo}>+91 6379603262</span> */}
-                </div>
                 <div className={`${styles.contacts} ${styles.flexCenter}`} data-aos="fade-up"  data-aos-delay="300">
                     <span className={`${styles.name} ${styles.pd-5}`}>Pranav M</span>
-                    <span className={`${styles.designation} ${styles.pd-5}`}>Finance</span> 
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Treasurer</span> 
                     <a href="mailto:finance.daksh@sastra.ac.in" className={styles.email}>finance.daksh@sastra.ac.in</a>   
                     {/* <span className={styles.phnNo}>+91 9841419109</span> */}
                 </div>
@@ -43,10 +37,16 @@ function Contact() {
                     {/* <span className={styles.phnNo}>+91 9840495111</span> */}
                 </div>
                 <div className={`${styles.contacts} ${styles.flexCenter}`} data-aos="fade-up"  data-aos-delay="500">
-                    <span className={`${styles.name} ${styles.pd-5}`}>Karthik Vason</span>
+                    <span className={`${styles.name} ${styles.pd-5}`}>Karthik Vason B</span>
                     <span className={`${styles.designation} ${styles.pd-5}`}>Head, Marketing and Shows</span> 
                     <a href="mailto:marketing.daksh@sastra.ac.in" className={styles.email}>marketing.daksh@sastra.ac.in</a>   
                     {/* <span className={styles.phnNo}>+91 7395958123</span> */}
+                </div>
+                <div className={`${styles.contacts} ${styles.flexCenter}`} data-aos="fade-up"  data-aos-delay="200">
+                    <span className={`${styles.name} ${styles.pd-5}`}>Achint Patro</span>
+                    <span className={`${styles.designation} ${styles.pd-5}`}>Overall Technical Coordinator</span> 
+                    <a href="mailto:lalitha.otcdaksh@gmail.com" className={styles.email}>otc.daksh@gmail.com</a>   
+                    {/* <span className={styles.phnNo}>+91 6379603262</span> */}
                 </div>
             </div>
         </div>
