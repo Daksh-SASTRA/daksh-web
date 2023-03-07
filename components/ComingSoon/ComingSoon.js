e8231c203d66b13fd9cef0ee29fc1e971ba2f0e9
Anirudh <anirudhsripathy3423@gmail.com>
2023-03-07 23:51:23 +0530

    Coming Soon component

---
+++ components/ComingSoon/ComingSoon.js
@@ -0,0 +1,10 @@
+import react from "react";
+import styles from "./comingsoon.module.css";
+
+function comingsoon() {
+  return (
+    <div className="container">
+      <h1>COMING SOON...</h1>
+    </div>
+  );
+}
