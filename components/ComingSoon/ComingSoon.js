b3fddd7c2701729aebc15f4cd8481d2dc6d74ebd
vishcomestrue <ramvishwa03@gmail.com>
2023-03-11 21:16:19 +0530

    Made minor changes to add responsiveness

--- components/ComingSoon/ComingSoon.js
+++ components/ComingSoon/ComingSoon.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./comingsoon.module.css";
 
-const ComingSoon =()=>{
-    let shadow = "";
+const ComingSoon = () => {
+  let shadow = "";
   for (let i = 0; i < 30; i++) {
-    shadow += `${(shadow ? "," : "") + -i * 0.3}px ${i * 0.4}px 0px #DA0037`;
+    shadow += `${(shadow ? "," : "") + -i * 0.3}px ${-i * 0.4}px 0px #DA0037`;
   }
 
   const shadowStyle = {
-    textShadow: shadow
+    textShadow: shadow,
   };
+
   return (
     <div className={styles.container}>
-    <div className={styles.text} style={shadowStyle}>
-    COMING
-    <br />
-    SOON...
-  </div>
+      <div className={styles.text} style={shadowStyle}>
+        COMING
+        <br />
+        SOON...
+      </div>
     </div>
   );
-}
+};
 
 export default ComingSoon;
