ad29753207a9ea83db5ec334389b7a388e410ba6
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-03-15 23:07:52 +0530

    Merch page completed.

--- components/Merch/Merch.js
+++ components/Merch/Merch.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react'
 import styles from './merch.module.css'
-import cstyles from '../Contact/contact.module.css';
-import Image from 'next/image';
 import MerchItem from "./Merchandise";
+import tshirt from '../../public/shirtpreview/tshirt.webp'
+import hoodie from '../../public/shirtpreview/hoodie.webp'
 
-import front from '../../public/shirtpreview/Front.png'
-import back from '../../public/shirtpreview/Back.png'
-
-import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
 
 function Merch() {
 
     return (
         <div className={styles.merch_container}>
-                <MerchItem merchName={"TSHIRT"} imageLink={front} shoppingLink="/merch/tshirt"/>
-                <MerchItem merchName={"HOODIE"} imageLink={back} shoppingLink="/merch/hoodie"/>
+                <MerchItem merchName={"TSHIRT"} imageLink={tshirt} shoppingLink="/merch/tshirt"/>
+                <MerchItem merchName={"HOODIE"} imageLink={hoodie} shoppingLink="/merch/hoodie"/>
         </div>
     )
 }
