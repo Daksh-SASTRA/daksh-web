3517918824d6556fda9a43bb87970f1df002b77e
VISHWANATH R <84440397+vishcomestrue@users.noreply.github.com>
2023-03-24 11:29:13 +0000

    Added hackathons page

---
+++ components/EventsWorkshop/Hackathons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Link from 'next/link';
+
+function Hackathons () {
+    return (
+        <div className={${styles.conatiner}}>
+            <h1>Hackathons</h1>
+        </div>
+    )
+}
+
+export default Hackathons
