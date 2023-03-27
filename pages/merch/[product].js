b1e8da8332651891cd61e226b39ff8e98eaeb27c
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-03-27 16:37:44 +0530

    added hoodie

--- pages/merch/[product].js
+++ pages/merch/[product].js
@@ -9,15 +9,18 @@ import hoodie_front from "../../public/shirtpreview/hoodie_front.webp";
 import hoodie_back from "../../public/shirtpreview/hoodie_back.webp";
 
 function ProductDetails() {
+  let hoodie_form = "https://forms.gle/fJd4TKBH7T2rGfBe7"
+  let t_form = "#"
   let router = useRouter();
   let name = router.query.product;
   let imageLinks = name == "tshirt" ? [tshirt_front, tshirt_back]: [hoodie_front, hoodie_back];
   let price = name == "tshirt" ? 250: 400;
-  name = name == "tshirt" ? "oversized tshirt":"Hoodie"; 
+  name = name == "tshirt" ? "oversized tshirt":"hoodie"; 
+  let form = name == "tshirt"?t_form: hoodie_form
   return (
     <main className={styles.main}>
       <Nav />
-      <Product productName={name} imageLinks={imageLinks} price={price}/>
+      <Product productName={name} imageLinks={imageLinks} price={price} buyNowLink={form}/>
       <Footer />
     </main>
   );
