ba01169bbd3a25c215fcad24e6f52f6ca8b8d13f
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-09-18 00:46:39 +0530

    minor bug fix

--- components/TeamCards/TeamCards.js
+++ components/TeamCards/TeamCards.js
@@ -28,25 +28,29 @@ function TeamCards() {
 				settings: {
 					centerMode: true,
 					centerPadding: "0px",
-					slidesToShow: 2,
+					slidesToShow: 3,
+					slidesToScroll: 3,
 				},
 			},
 			{
 				breakpoint: 800,
 				settings: {
-					centerMode: true,
+					centerMode: false,
 					centerPadding: "0px",
 					slidesToShow: 1,
+					slidesToScroll: 1,
 				},
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					centerMode: true,
-					centerPadding: "0px",
-					slidesToShow: 1,
-				},
-			},
+			}
+			// ,
+			// {
+			// 	breakpoint: 480,
+			// 	settings: {
+			// 		centerMode: false,
+			// 		centerPadding: "0px",
+			// 		slidesToShow: 1,
+			// 		slidesToScroll: 1,
+			// 	},
+			// },
 		],
 	};
 
